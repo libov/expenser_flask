5df615b58341b4bd4a608958eaa7d56405a58c75
Vladistar Libor <hammer10@Desktop>
2020-10-11 14:06:21 +0200

    implemented readout from the original xml file. Removed model class definitions (now in a separate file)

--- initialise_database.py
+++ initialise_database.py
@@ -1,59 +1,74 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
+import xml.etree.ElementTree as ET
+import datetime
+from datamodel import *
 
 app = Flask(__name__)
+app.app_context().push() # this is to prevent context errors
 
 app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://testuser:testpassword@localhost/expenses'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-db = SQLAlchemy(app)
+db.init_app(app)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
 
-class Balance(db.Model):
-    __tablename__ = 'BALANCE'
-    id = db.Column(db.Integer, primary_key = True)
-    amount = db.Column(db.Float(precision=2), nullable=False)
-    date = db.Column(db.Date, nullable=False, unique=True)
-
-class Category(db.Model):
-    __tablename__ = 'CATEGORY'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(64), unique = True, nullable=False)
-    expenses = db.relationship("Cashflow", backref='category')
-    
-    def __repr__(self):
-        return '<Category %r>' % self.name
-
-class Cashflow(db.Model):
-    __tablename__ = 'CASHFLOW'
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(256), nullable=False)
-    amount = db.Column(db.Float(precision=2), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    booked = db.Column(db.Boolean, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('CATEGORY.id'), nullable=False)
-
-    def __repr__(self):
-        return '<Cashflow id %r>' % self.id
-    
-if __name__ == '__main__':
+def initialise_database():
     db.drop_all()
     db.create_all()
-    #admin_role=Role(name='Admin')
-    #mod_role=Role(name='Moderator')
-    #user_john=User(username='john', rolee=mod_role)
-    #user_susan=User(username='susan', rolee=mod_role)
-    #user_susan2=User(username='susan2', rolee=admin_role)
-    #db.session.add(admin_role)
-    #db.session.add(mod_role)
-    #db.session.add(user_john)
-    #db.session.add(user_susan)
-    #db.session.add(user_susan2)
-    #db.session.delete(admin_role)
+
+def migrate_database():
+    
+    # read the source XML-database
+    tree = ET.parse('expenses.xml')
+    root = tree.getroot()
+    i=0
+
+    # as the first step we need to write the categories, because it is used
+    # as a foreign key
+
+    # first, determine the categories
+    categories = []
+    for expense in root:
+        if expense.tag != "expense": continue
+        category =  expense.find("category").text
+        if category not in categories:
+            categories.append(category)
+    categories.sort()
+
+    # write to the database
+    for category in categories:
+        cat = Category(name=category)
+        db.session.add(cat)
+
+    # now write the expenses
+    for expense in root:
+        if expense.tag != "expense": continue
+        amount = expense.find("amount").text
+        day = int(expense.find("date/day").text)
+        month = int(expense.find("date/month").text)
+        year = int(expense.find("date/year").text)
+        withdrawn = expense.find("withdrawn").text
+        category =  expense.find("category").text
+        description =  expense.find("description").text
+
+        # to avoid errors when there is no description
+        if description == None:
+            description=""
+
+        booked=False
+        if withdrawn == "Yes":
+            booked = True
+
+        cat = Category.query.filter_by(name=category).first()
+
+        cf = Cashflow(amount=amount, description=description, booked=booked, date=datetime.date(year, month, day), category=cat)
+
+        db.session.add(cf)
+        print("added ", amount)
+
+if __name__ == '__main__':
+
+    initialise_database()
+    migrate_database()
     db.session.commit()
-    
-    #print(Role.query.all())
-    #print(User.query.all())
-    #print("Admins:", User.query.filter_by(rolee=admin_role).all())
-   # print("Moderators:", User.query.filter_by(rolee=mod_role).all())
-    
